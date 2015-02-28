42884e7eccc3f1ba07aff041b74e8e7c0bd22497
ManthenaMurali <manthena.android@gmail.com>
2015-02-28 11:09:32 +0530

    first commit

---
+++ src/com/bmm/camerarecording/CameraRecordingActivity.java
@@ -0,0 +1,14 @@
+package com.bmm.camerarecording;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CameraRecordingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_camera_recording);
+		
+	}
+}
